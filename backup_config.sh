6b8dac410f19880cd61c8b4a3861e8bc797f4cba
Petrus <silencly07@gmail.com>
2018-04-03 14:41:29 +0800

    修改backup目录结构，添加备份文件

--- backup_config.sh
+++ backup_config.sh
@@ -7,11 +7,20 @@ set -o pipefail
 backup_dir="/home/petrus/Backup/config_files"
 config_list="/home/petrus/Backup/config_list.conf"
 
+if [ ! -d $backup_dir ]; then
+  mkdir $backup_dir
+fi
+
 cat $config_list | grep -Ev "^$|#" | while read line
 do
-    # 获取文件名
+    # 获取目录和文件名
     name=$(echo $line | awk -F '/' '{print $NF}')
-    cp -aL $line $backup_dir
+    dir=$(echo $line | sed "s/\/${name}$//g" | awk '{print substr($1,2)}')
+    if [ ! -d $backup_dir/$dir ]; then
+        mkdir -p $backup_dir/$dir
+    fi
+    cp -aL $line $backup_dir/$dir
+    # echo "cp -aL $line $backup_dir/$dir"
 done
 
 touch $backup_dir/backup_stamp

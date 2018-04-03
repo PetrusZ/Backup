6a7cb31c95b9a6e73d1812d97b6ed4d20a3f7fa0
Petrus <silencly07@gmail.com>
2018-04-03 14:39:18 +0800

    删除旧格式配置

--- config_files/Scripts/fonts.sh
+++
@@ -1,4 +0,0 @@
-for family in serif sans-serif monospace Arial Helvetica Verdana "Times New Roman" "Courier New"; do
-  echo -n "$family: "
-  fc-match "$family"
-done
--- config_files/Scripts/install_cquery_cmd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ~/.vim/cquery
-./waf configure --llvm-config=/usr/lib/llvm/5/bin/llvm-config --prefix=~/.local  build
-./waf install

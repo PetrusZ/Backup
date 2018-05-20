58396fed3c1e7302ab254adffd0a11348afc603c
Petrus <silencly07@gmail.com>
2018-05-20 20:51:02 +0800

    weekly update by crontab script

--- push2github.sh
+++ push2github.sh
@@ -10,5 +10,5 @@ source /home/petrus/Backup/backup_config.sh
 cd $root_dir
 
 git add .
-git commit -m "daily update by crontab script" > /dev/null
-git push > /dev/null
+git commit -m "weekly update by crontab script" > /dev/null
+git push &> /dev/null
